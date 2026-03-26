16eb2cb338d90f211f524ce21b1b279003066904
Ot4man <melloukiotmane@gmail.com>
2026-03-26 19:15:00 +0100

    feat:add authentification routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/register/eligibility', [AuthController::class, 'showEligibility'])->name('eligibility');
+Route::post('/register/eligibility', [AuthController::class, 'checkEligibility'])->name('eligibility.check');
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function() {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
